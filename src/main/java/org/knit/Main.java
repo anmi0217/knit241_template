32f2818cf11e33eeec9c4106111705f575a6f5cb
alkogol <unknowngy@gmail.com>
2024-09-15 20:25:32 +0400

    init

---
+++ src/main/java/org/knit/Main.java
@@ -0,0 +1,10 @@
+package org.knit;
+
+import org.knit.lab1.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        task1.execute();
+    }
+}
--- src/main/java/org/knit/lab1/Task1.java
+++ src/main/java/org/knit/lab1/Task1.java
@@ -16,10 +16,12 @@ package org.knit.lab1;
  * 71,72,73,74,75,76,77,78,79,80
  * 81,82,83,84,85,86,87,88,89,90
  * 91,92,93,94,95,96,97,98,99,100
+ *
  ***/
 public class Task1 {
-    public static void main(String[] args) {
-        /**
+
+    public void execute() {
+        /*
          * Ваше решение
          */
     }
